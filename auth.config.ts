7c904732af045227ee053d925fceb6798b8f629b
Quiquia <vanebequi1994@gmail.com>
2024-12-20 12:05:53 -0500

    add output server

--- auth.config.ts
+++ auth.config.ts
@@ -1,11 +1,11 @@
 // import Twitch from "@auth/core/providers/twitch";
-// import { defineConfig } from "auth-astro";
+import { defineConfig } from "auth-astro";
 
-// export default defineConfig({
-//   providers: [
-//     Twitch({
-//       clientId: import.meta.env.TWITCH_CLIENT_ID,
-//       clientSecret: import.meta.env.TWITCH_CLIENT_SECRET,
-//     }),
-//   ],
-// });
+export default defineConfig({
+  providers: [
+    // Twitch({
+    //   clientId: import.meta.env.TWITCH_CLIENT_ID,
+    //   clientSecret: import.meta.env.TWITCH_CLIENT_SECRET,
+    // }),
+  ],
+});
